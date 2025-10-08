0bd30753e15f43eece9830d95e10ec5b93c0760b
Ragavi-byte <raguragavi6@gmail.com>
2025-10-08 09:28:47 +0530

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,22 @@
+//package org.example;
+//import java.util.ArrayList;
+//import java.util.List;
+//public class arraylist {
+//    public static void main(String[] args) {
+//        ArrayList<Integer> list = new ArrayList<>();
+//        list.add(5);
+//        list.add(20);
+//        list.add(42);
+//        list.add(35);
+//        System.out.println(list);
+//        for(int i = 0; i < list.size(); i++) {
+//            int num = list.get(i);
+//            if (num % 5 == 0) {
+//                System.out.println("divisible by 5: " + num);
+//            }
+//            if (num % 7 == 0) {
+//                System.out.println("divisible by 7:" + num);
+//            }
+//        }
+//    }
+//}
---
+++ sevenbyten.java
@@ -0,0 +1,46 @@
+//package org.example;
+//
+//import java.util.ArrayList;
+
+//import java.util.ArrayList;
+//
+//public class sevenbyten {
+//    public static void main(String[] args) {
+//        ArrayList<String> list = new ArrayList<>();
+//        list.add("hello");
+//        list.add("world");
+//        System.out.println(list);
+//        list.set(1,"java");
+//        System.out.println(list);
+//        list.remove(0);
+//        System.out.println(list);
+//        System.out.println(list.size());
+//        list.get(0);
+//        System.out.println(list);
+//        System.out.println(list.contains("java"));
+//        list.remove("java");
+//        System.out.println(list);
+//        list.clear();
+//        System.out.println(list);
+//        System.out.println(list.isEmpty());
+//
+//
+//    }
+//}
+//public class Arraylist{
+//    public static void main(String[] args) {
+//        ArrayList<Integer> list =new ArrayList<>();
+//        list.add(5);
+//        list.add(20);
+//        list.add(42);
+//        list.add(35);
+//        System.out.println(list);
+//        for(int i=0;i<list.size();i++);
+//        if(i%5==0) {
+//            System.out.println("divisible by 5"+i);
+//        }
+//        if(i%7==0){
+//            System.out.println("divisible by 7"+i);
+//        }
+//    }
+//}

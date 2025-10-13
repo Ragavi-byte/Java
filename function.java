d807bee13e030b61097afacf6f9efd748254bfa7
Ragavi-byte <raguragavi6@gmail.com>
2025-10-13 13:45:33 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,25 @@
+//package function;
+//
+//interface A{
+//    void add(int a,int b);
+//}
+//interface B{
+//    void multiply(int a,int b);
+//}
+//class C implements A,B{
+//    @Override
+//    public void add(int a,int b) {
+//        System.out.println("a+b:"+(a+b));
+//    }
+//    @Override
+//    public void multiply(int a,int b) {
+//        System.out.println("a*b:"+(a*b));
+//    }
+//}
+//class main{
+//    public static void main(String[] args) {
+//        C c= new C();
+//        c.add(5,9);
+//        c.multiply(4,4);
+//    }
+//}
